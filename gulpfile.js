d00a491cd60a6144059beb1ad304cfef96ccdfb8
Korroz <korroz.with.git@voidentity.net>
2015-04-16 23:18:29 +0200

    Concatenate nomnoml script using gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var nomnomlFiles = [
+    'lib/underscore.skanaar.js',
+    'lib/skanaar.canvas.js',
+    'nomnoml.vectorMath.js',
+    'nomnoml.jison.js',
+    'nomnoml.parser.js',
+    'nomnoml.layouter.js',
+    'nomnoml.renderer.js',
+    'nomnoml.js'
+]
+
+gulp.task('default', function () {
+    gulp.src(nomnomlFiles)
+        .pipe(concat('nomnoml.js'))
+        .pipe(gulp.dest('dist/'));
+});
