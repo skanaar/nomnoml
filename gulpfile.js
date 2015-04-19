b8ef191f49f721b3d7fa1089bec7e5c36850991f
Korroz <korroz.with.git@voidentity.net>
2015-04-19 18:41:04 +0200

    Make nomnoml compatible with RequireJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
 
 var nomnomlFiles = [
     'lib/_skanaar.js',
@@ -10,10 +12,19 @@ var nomnomlFiles = [
     'nomnoml.layouter.js',
     'nomnoml.renderer.js',
     'nomnoml.js'
-]
+];
+var ftr = [
+    '',
+    '\tif (typeof define === "function" && define.amd) define(nomnoml);',
+    '\telse if (typeof module === "object" && module.exports) module.exports = nomnoml;',
+    '\tthis.nomnoml = nomnoml;',
+    '})();'
+].join('\n');
 
 gulp.task('default', function () {
     gulp.src(nomnomlFiles)
         .pipe(concat('nomnoml.js'))
+        .pipe(header('(function () {\n'))
+        .pipe(footer(ftr))
         .pipe(gulp.dest('dist/'));
 });
