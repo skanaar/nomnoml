044cc00ef725593170698a2cdde636dd45222149
Ferdinand Prantl <prantlf@gmail.com>
2015-07-30 21:03:20 +0200

    Allow usage in NodeJS with the embedded dagre module
    The dagre module detects the module exports in the NodeJS environment and
    does not declare the dagre object globally.  The code in nomnoml.layouter.js
    fails accessing the global dagre variable.  Let's declate self = {} to make
    dagre put its object to it, when running outside of the browser.
    
    A better solution would be to require the dagre module like lodash and
    limiting the supported versions to 0.4.x, which have the needed interface.

--- bundleWrapper.js
+++ bundleWrapper.js
@@ -5,6 +5,14 @@
   	module.exports = factoryFn(require('lodash'));
   else this.nomnoml = factoryFn(_);
 })(function (_) {
+  var self; // Enable usage outside of the browser (in NodeJS)
+  if (typeof self === 'undefined' && typeof window === 'undefined') {
+    self = {};
+  }
   /*{{body}}*/;
+  var dagre; // Enable usage outside of the browser (in NodeJS)
+  if (typeof dagre === 'undefined' && typeof window === 'undefined') {
+    dagre = self.dagre;
+  }
   return nomnoml;
 });
