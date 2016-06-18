0ec91f01bf01e356d5f95dfde28e377ad3080375
skanaar <daniel.kallin@gmail.com>
2016-06-18 18:37:20 +0200

    command line interface with #import support

---
+++ dist/nomnoml-cli.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+var fs = require('fs')
+var nomnoml = require('./nomnoml.js')
+
+var args = process.argv
+
+if (args[2] == '--help' || args.length == 2){
+  console.log(`
+  Load source file and send rendered svg to stdout
+
+  nomnoml <source_file>
+
+  Load source file and save rendered svg to <output_file>
+  
+  nomnoml <source_file> <output_file>
+
+  Third parameter overrides the import depth limit
+  that protects us from recursive imports
+
+  nomnoml <source_file> <output_file> <max_import_chain_length>`)
+  return
+}
+
+var maxImportChainLength = args[4] || 10
+
+var svg = nomnoml.renderSvg(preprocessFile(args[2], 0))
+if (args[3]){
+  fs.writeFileSync(args[3], svg)
+}
+else {
+  console.log(svg)
+}
+
+function preprocessFile(filepath, depth){
+  if (depth > maxImportChainLength)
+    throw Error('max_import_chain_length exceeded')
+  var source = fs.readFileSync(filepath, {encoding:'utf8'})
+  return source.replace(/#import: *(.*)/g, function (a, file) {
+    return preprocessFile(file, depth+1)
+  })
+}
