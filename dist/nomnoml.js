5de9ed39ba0622c34a869cbe4f13954f6e5ea989
strobox <afram555@bk.ru>
2019-01-22 12:10:59 +0300

    remove CommonJS jison-parser exports.

--- dist/nomnoml.js
+++ dist/nomnoml.js
@@ -1077,25 +1077,7 @@ function Parser () {
 }
 Parser.prototype = parser;parser.Parser = Parser;
 return new Parser;
-})();
-
-
-if (typeof require !== 'undefined' && typeof exports !== 'undefined') {
-exports.parser = nomnomlCoreParser;
-exports.Parser = nomnomlCoreParser.Parser;
-exports.parse = function () { return nomnomlCoreParser.parse.apply(nomnomlCoreParser, arguments); };
-exports.main = function commonjsMain (args) {
-    if (!args[1]) {
-        console.log('Usage: '+args[0]+' FILE');
-        process.exit(1);
-    }
-    var source = require('fs').readFileSync(require('path').normalize(args[1]), "utf8");
-    return exports.parser.parse(source);
-};
-if (typeof module !== 'undefined' && require.main === module) {
-  exports.main(process.argv.slice(1));
-}
-};
+})();;
 var nomnoml = nomnoml || {}
 
 _.maxBy = _.maxBy || _.max // polyfill the differences between lodash and underscore
--- src/jison-parser.js
+++ src/jison-parser.js
@@ -616,21 +616,3 @@ function Parser () {
 Parser.prototype = parser;parser.Parser = Parser;
 return new Parser;
 })();
-
-
-if (typeof require !== 'undefined' && typeof exports !== 'undefined') {
-exports.parser = nomnomlCoreParser;
-exports.Parser = nomnomlCoreParser.Parser;
-exports.parse = function () { return nomnomlCoreParser.parse.apply(nomnomlCoreParser, arguments); };
-exports.main = function commonjsMain (args) {
-    if (!args[1]) {
-        console.log('Usage: '+args[0]+' FILE');
-        process.exit(1);
-    }
-    var source = require('fs').readFileSync(require('path').normalize(args[1]), "utf8");
-    return exports.parser.parse(source);
-};
-if (typeof module !== 'undefined' && require.main === module) {
-  exports.main(process.argv.slice(1));
-}
-}
