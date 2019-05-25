f2bf98a23d4c0d15f2bfdc13a091e85154c5b1b9
skanaar <daniel.kallin@gmail.com>
2019-05-25 13:06:17 +0200

    update build pipe

--- build.js
+++ build.js
@@ -18,28 +18,10 @@ fs.writeFileSync('dist/nomnoml.js', bundle)
 fs.writeFileSync('dist/nomnoml.web.js', dagreSrc + ';' + bundle)
 fs.unlinkSync('dist/nomnoml.compiled.js')
 
-try {
-    assertLibraryVersion()
-    require('./test/render-svg.js')
-    require('./test/nomnoml.spec.js')
-}
-catch(e) {
-    fs.unlinkSync('dist/nomnoml.js', bundle)
-    throw e
-}
-
 function read(file){
     return fs.readFileSync(file, { encoding: 'utf8' })
 }
 
 function replace(source, token, replacement){
     return source.split(token).join(replacement)
-}
-
-function assertLibraryVersion() {
-    var library = require('./dist/nomnoml.js')
-    var package = require('./package.json')
-    if (library.version != package.version) {
-        throw new Error('version of distribution bundle and npm package must match')
-    }
 }
---
+++ test.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+try {
+    assertLibraryVersion()
+    require('./test/render-svg.js')
+    require('./test/nomnoml.spec.js')
+}
+catch(e) {
+    fs.unlinkSync('dist/nomnoml.js', bundle)
+    throw e
+}
+
+function assertLibraryVersion() {
+    var library = require('./dist/nomnoml.js')
+    var package = require('./package.json')
+    if (library.version != package.version) {
+        throw new Error('version of distribution bundle and npm package must match')
+    }
+}
