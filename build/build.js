b05feb3636f3e18591ff5c664866e07b0eaa1fd2
skanaar <daniel.kallin@gmail.com>
2020-09-14 08:34:55 +0200

    better fallback behaviour in build caching

--- build/build.js
+++ build/build.js
@@ -3,22 +3,25 @@ var jison = require('jison');
 var uglify = require('uglify-js');
 
 var package = require('../package.json');
-var dagreMinified = 'cache/dagre-' + package.dependencies.dagre + '.json';
+var dagreMinifiedFile = 'cache/dagre-' + package.dependencies.dagre + '.js';
+var dagreMinified = ''
 
 // minification is slow so only run this on-demand
-if (!fs.existsSync(dagreMinified)) {
-    var dagreRawSrc = read('node_modules/dagre/dist/dagre.min.js')
-    fs.writeFileSync(dagreMinified, uglify.minify(dagreRawSrc).code);
+if (fs.existsSync(dagreMinifiedFile)) {
+    dagreMinified = read(dagreMinifiedFile)
+} else {
+    dagreMinified = uglify.minify(read('node_modules/dagre/dist/dagre.min.js')).code
+    fs.mkdirSync('cache', { recursive: true })
+    fs.writeFileSync(dagreMinifiedFile, dagreMinified)
 }
 
-var dagreSrc = read(dagreMinified)
 var grammar = new jison.Parser(read('src/nomnoml.jison'));
 var parser = grammar.generate({moduleName: 'nomnomlCoreParser',moduleType:'js'})
 var source = read('dist/nomnoml.compiled.js') + ';\n' + parser
 var wrapper = read('build/umd-bundle.js.template')
 var bundle = replace(wrapper, '/*{{body}}*/', source)
 fs.writeFileSync('dist/nomnoml.js', bundle)
-fs.writeFileSync('dist/nomnoml.web.js', dagreSrc + ';' + bundle)
+fs.writeFileSync('dist/nomnoml.web.js', dagreMinified + ';' + bundle)
 fs.unlinkSync('dist/nomnoml.compiled.js')
 
 function read(file){
