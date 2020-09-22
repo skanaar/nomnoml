172a0c84a63791a2c0acf0cf921edd691403d329
skanaar <daniel.kallin@gmail.com>
2020-09-22 11:02:53 +0200

    type definitions

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export var version: string;
+export function draw(canvas: HTMLCanvasElement, code: string, scale?: number): {}
+export function renderSvg(code: string, document?: HTMLDocument): string
+export function compileFile(filepath: string, maxImportDepth?: number): string
