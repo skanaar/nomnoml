d6f327b280734db85d299934b92aa8348b1fcf84
skanaar <daniel.kallin@gmail.com>
2020-09-22 15:42:37 +0200

    tweaked export menu

--- webapp/export-menu.comp.ts
+++ webapp/export-menu.comp.ts
@@ -2,20 +2,22 @@ function ExportMenu(props: { app: App }) {
   var downloader = props.app.downloader
   var sourceCode = props.app.downloader.source
   return div({ className: "file-menu" },
+    h2({}, 'Share diagram'),
     a({ className: 'btn', href: '#view/' + Route.urlEncode(sourceCode), target: '_blank' },
-      el(Icon, { id: 'link-outline' }), 'Open shareable link'
+      el(Icon, { id: 'link-outline' }), 'Shareable link'
     ),  
     a({ className: 'btn', href: 'image.svg?source=' + Route.urlEncode(sourceCode), target: '_blank' },
-      el(Icon, { id: 'globe' }), 'Open linkable SVG'
+      el(Icon, { id: 'globe' }), 'Server hosted SVG'
     ),
+    h2({}, 'Downloads'),
     a({ className: 'btn', href: '/', onClick: prevent(() => downloader.pngDownload()) },
-      el(Icon, { id: 'camera-outline' }), 'Download PNG'
+      el(Icon, { id: 'camera-outline' }), 'PNG image'
     ),
     a({ className: 'btn', href: '/', onClick: prevent(() => downloader.svgDownload()) },
-      el(Icon, { id: 'image-outline' }), 'Download SVG with source'
+      el(Icon, { id: 'image-outline' }), 'SVG with source'
     ),
     a({ className: 'btn', href: '/', onClick: prevent(() => downloader.srcDownload()) },
-      el(Icon, { id: 'download-outline' }), 'Download source'
+      el(Icon, { id: 'download-outline' }), 'Source code'
     ),
   )
 }
