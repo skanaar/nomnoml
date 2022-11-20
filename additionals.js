d1d6beda67763194f0a7daddb6bc886621124c9b
skanaar <daniel.kallin@gmail.com>
2022-11-20 13:08:30 +0100

    add nullitics cookieless tracking

--- additionals.js
+++ additionals.js
@@ -11,5 +11,10 @@ try {
   ga('send', 'pageview');
 } catch(e) {}
 
+new Image().src='https://nullitics.com/null.gif'+
+  '?u='+encodeURI(location.href)+
+  '&r='+encodeURI(document.referrer)+
+  '&d='+screen.width
+
 // -- daily tip ---------------------
 
